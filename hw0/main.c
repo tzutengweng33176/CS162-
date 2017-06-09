cca740ec9da56ac80a6bf38af08b257a757871f8
Tzu-teng. Weng <tzutengweng33176@gmail.com>
2017-06-09 06:44:03 +0000

    Finished my first CS162 assignment

--- hw0/main.c
+++ hw0/main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <sys/resource.h>
+#include <sys/time.h>
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    getrlimit(RLIMIT_STACK, &lim);
+
+    long int stacksize=lim.rlim_cur;
+    getrlimit(RLIMIT_NPROC, &lim);
+
+    long int processlimit=lim.rlim_cur;
+    
+    getrlimit(RLIMIT_NOFILE, &lim);
+    long int maxfiledescriptor=lim.rlim_cur;
+
+
+    printf("stack size: %ld\n", stacksize);
+    printf("process limit: %ld\n", processlimit);
+    printf("max file descriptors: %ld\n", maxfiledescriptor);
+
+
     return 0;
 }
